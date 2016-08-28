c28d4c63ff93c5a9c4ea7b0258528c1d82bd9ccc
Anti-k <romanslyzebniy@gmail.com>
2016-08-28 14:00:30 +0300

    welcome#index refactored

--- app/uploaders/front_uploader.rb
+++ app/uploaders/front_uploader.rb
@@ -41,6 +41,11 @@ class FrontUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [70, 70]
   end
 
+  version :full do
+    process :resize_to_fit => [120, 120]
+  end
+
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
