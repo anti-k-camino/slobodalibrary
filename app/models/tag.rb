1334f26e024ab1bd800299ea8f369fa9960f00c4
Anti-k <romanslyzebniy@gmail.com>
2016-08-29 03:27:17 +0300

    spec model set

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ActiveRecord::Base
   has_many :books_tag
-  has_many :books, through: :books_tag
+  has_many :books, through: :books_tag  
 end
--- spec/models/books_tag_spec.rb
+++ spec/models/books_tag_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe BooksTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it { should have_db_index([:book_id, :tag_id]).unique(true)}
+
 end
